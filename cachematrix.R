6e4c05db4bb61d8b92cd2043b86c3bc1247d4201
Mark Moore <Blue-Leader@users.noreply.github.com>
2015-09-22 13:33:22 +0100

    cleaned up for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,10 @@
-## This R file contains functions that will cache an inverse matrix, and solve
-## the inverse of the matrix if it is not already cached.
+## This R file contains functions that will cache the inverse of a matrix, and 
+## solve the matrix if the inverse is not already cached.
 
 ## This function defines 4 functions that set and return the values of a matrix
-## and its inverse.  It also creates a list of these functions.  
-## V4 - debugging complete - TODO: test with larger matrices. 
+## and its inverse.  It also creates and returns a list of these functions.  
+## 
+## V5 - debugging complete - tested with larger matrices. 
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -30,27 +31,30 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-## This function checks the value of the stored inverse. If it is null,
-## the function calculates and caches the inverse in the the main function.
+## This function checks the value of the stored inverse matrix. If it is NULL,
+## the function calculates (solves) and caches the inverse in the main function.
+## It then returns the inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Check the value of the stored inverse and return it if not NULL
-        ## message("right here")
-        ## print(class(x))
+        ## Check the value of the stored inverse and return it IF (not NULL)
+        
         i <- x$getinverse()
         if (!is.null(i)) {
                 message("Getting cached inverse...")
                 return(i)
         }
         
-        ## ELSE: Calulate and return a matrix that is the inverse of 'x'
+        ## ELSE: Calculate and return a matrix that is the inverse of 'x'
         data <- x$get()
         
-        ## solve the matrix to get the inverse - if no arg specified, solve() 
-        ## takes it to mean we want the identity
+        ## solve the matrix to get the inverse - if no second arg specified, 
+        ## solve() takes it to mean we want the identity and thus returns 
+        ## the inverse matrix.
+        
         matinverse <- solve(data, ...)
         
-        ## cache and return the result
+        ## cache and return the inverse matrix
+        
         x$setinverse(matinverse)
         matinverse
         
